f5dc43e2d51cfef9cc1c79cb51bc985056220476
El <richard.black183@gmail.com>
2022-04-27 09:48:07 +0300

    +add electron b

--- client/src/api/ApolloClient.js
+++ client/src/api/ApolloClient.js
@@ -2,5 +2,5 @@ import ApolloClient from "apollo-boost";
 
 const apolloClient = new ApolloClient({
   // You should use an absolute URL here
-  uri: "https://localhost:4000/graphql",
+  uri: process.env.BASE_URL,
 });
